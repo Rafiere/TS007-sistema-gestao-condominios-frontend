464e86dda977205c8210e1caffe7c1b4d83148b6
Rafael <rafapgbr@gmail.com>
2022-09-26 12:53:07 -0300

    feat: inserção do aplicativo básico.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
-function App() {
-    return <></>;
-}
+import "./shared/forms/TraducoesYup";
+import { Register } from "./shared/components/register/Register";
+import { AppThemeProvider } from "./shared/contexts/ThemeContext";
+import { AuthProvider } from "./shared/contexts/AuthContext";
+
+export const App = () => {
+    return (
+        <AuthProvider>
+            <AppThemeProvider>
+                <Register>
+                    <></>
+                </Register>
+            </AppThemeProvider>
+        </AuthProvider>
+    );
+};
 
 export default App;
